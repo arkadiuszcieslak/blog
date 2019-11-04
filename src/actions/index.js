1f0b8ed899ed61135cc356f646192d12ea71adcc
Arkadiusz Cieslak <arekc70@gmail.com>
2019-11-04 22:01:52 +0100

    Improved fetching users - fetching unique users only.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,21 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
 
-    dispatch({ type: 'FETCH_POSTS', payload: response.data });
-  };
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
 
 export const fetchUser = (userId) => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${userId}`);
+  const response = await jsonPlaceholder.get(`/users/${userId}`);
 
-    dispatch({ type: 'FETCH_USER', payload: response.data });
-  };
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
